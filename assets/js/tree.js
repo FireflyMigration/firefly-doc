a8dedc2b5910f933a41b6cc3e88574a0b08e5655
Noam Honig <noam@fireflymigration.com>
2017-02-03 11:29:23 +0200

    Completed search capabilities

--- assets/js/tree.js
+++ assets/js/tree.js
@@ -5,11 +5,21 @@ function buildTree(id, currentPageUrl, backButtonId, nextButtonId) {
     var root = document.getElementById(id);
     var backBtn = document.getElementById(backButtonId);
     var nextBtn = document.getElementById(nextButtonId);
-    backBtn.style.visibility = 'hidden';
-    nextBtn.style.visibility = 'hidden';
+    function foreach(array, action) {
+        if (array != undefined)
+            for (var i = 0; i < array.length; i++)
+                action(array[i]);
+    };
+
     function buttonHide(b) {
         b.style.visibility = 'hidden';
     }
+    function nodeShow(n) {
+        n.style.display = 'block';
+    };
+    function nodeHide(n) {
+        n.style.display = 'none';
+    };
     buttonHide(backBtn);
     buttonHide(nextBtn);
 
@@ -18,77 +28,188 @@ function buildTree(id, currentPageUrl, backButtonId, nextButtonId) {
         b.href = url;
     }
 
-    var prevUrl = undefined;
+    var prevUrl = '/';
     var doNextBtn = false;
 
+    function search(nodes, what) {
+        var found = false;
+        foreach(nodes, function (item) {
 
-    function registerClick(on, from) {
-        on.onclick = function () {
-            if (from.style.display == 'block') {
-                from.style.display = 'none';
-                on.innerText = '+';
+            var imFound = false;
+            if (search(item.nodes, what)) {
+                imFound = true;
             }
-            else {
-                from.style.display = 'block';
-                on.innerText = '-';
+            if (item.matches(what)) {
+                imFound = true;
             }
-        };
+            if (imFound) {
+                item.expand();
+                item.show();
+                found = true;
+
+            }
+
+
+
+
+
+        });
+        return found;
     }
+    function reset(nodes) {
+        var found = false;
+        foreach(nodes, function (item) {
+            if (item.resetAndReturnTrueIfYouAreSelected())
+                found = true;
+            if (item.nodes != undefined) {
+                if (reset(item.nodes)) {
+                    found = true;
+                    item.expand();
+                }
+            }
+        });
+        return found;
+    }
+
+
 
     function buildNodes(nodes, parent) {
         var found = false;
-        for (var i = 0; i < nodes.length; i++) {
-            var item = nodes[i];
-            var treeItem = document.createElement('div');
-            treeItem.classList.add('TreeItem');
+        foreach(nodes,
+            function (item) {
+                var treeItem = document.createElement('div');
+                treeItem.classList.add('TreeItem');
 
-            var treeTitle = document.createElement('a');
-            if (item.url != undefined) {
-                treeTitle.href = item.url;
-                if (doNextBtn) {
-                    buttonUrl(nextBtn, treeTitle.href);
-                    doNextBtn = false;
+                var plusMinusButton = document.createElement('div');
+                plusMinusButton.style.width = '1em';
+                plusMinusButton.style.display = 'inline-block';
+                treeItem.appendChild(plusMinusButton);
+
+                var treeTitle = document.createElement('a');
+                treeTitle.innerText = item.name;
+                treeItem.appendChild(treeTitle);
+
+
+
+                if (item.url != undefined) {
+                    treeTitle.href = item.url;
+                    if (doNextBtn) {
+                        buttonUrl(nextBtn, treeTitle.href);
+                        doNextBtn = false;
+                    }
+                    if (item.url == currentPageUrl) {
+                        found = true;
+                        treeItem.classList.add('current-page');
+                        buttonUrl(backBtn, prevUrl);
+                        doNextBtn = true;
+                    }
+                    prevUrl = treeTitle.href;
                 }
-                if (item.url == currentPageUrl) {
-                    found = true;
-                    treeItem.classList.add('current-page');
-                    buttonUrl(backBtn, prevUrl);
-                    doNextBtn = true;
+
+
+                item.lowerName = item.name.toLowerCase();
+                item.matches = function (what) {
+                    var i = item.lowerName.indexOf(what);
+                    if (i === -1) {
+                        treeTitle.innerText = item.name;
+                        item.hide();
+                        item.collapse();
+                        return false;
+                    }
+                    else {
+                        item.show();
+                        var before = item.name.substring(0, i);
+                        var middle = item.name.substr(i, what.length);
+                        var after = item.name.substr(i + what.length, item.name.length - i - what.length);
+                        treeTitle.innerHTML = before + '<strong>' + middle + '</strong>' + after;
+                        return true;
+                    }
+                };
+                item.resetAndReturnTrueIfYouAreSelected = function () {
+                    treeTitle.innerText = item.name;
+                    item.show();
+                    item.collapse();
+                    if (item.url == currentPageUrl) {
+                        item.expand();
+                        return true;
+                    }
+                    else {
+                        item.collapse();
+                        return false;
+                    }
+
+
                 }
-                prevUrl = treeTitle.href;
-            } 
-            treeTitle.innerText = item.name;
-            treeItem.appendChild(treeTitle);
+
+                item.collapse = function () { };
+                item.expand = function () { };
+                item.hide = function () {
+                    nodeHide(treeItem);
+                    item.collapse();
+                };
+                item.show = function () {
+                    nodeShow(treeItem);
+                    item.expand();
+                };
+                parent.appendChild(treeItem);
+
+                if (item.nodes != undefined) {
+                    var childNodesContainer = document.createElement('div');
+                    childNodesContainer.classList.add('TreeNode');
+                    parent.appendChild(childNodesContainer);
+
+                    item.collapse = function () {
+                        plusMinusButton.innerText = '+';
+                        nodeHide(childNodesContainer);
+                    };
+                    item.expand = function () {
+                        plusMinusButton.innerText = '-';
+                        nodeShow(childNodesContainer);
+                    };
+                    item.toggle = function () {
+                        if (childNodesContainer.style.display == 'block')
+                            item.collapse();
+                        else
+                            item.expand();
+                    };
+                    if (item.url == currentPageUrl)
+                        item.expand();
+                    else
+                        item.collapse();
+                    plusMinusButton.onclick = item.toggle;
+
+                    if (buildNodes(item.nodes, childNodesContainer)) {
+                        item.expand();
+                        found = true;
+                    }
 
 
-            var plusMinusButton = document.createElement('div');
-            plusMinusButton.style.width = '1em';
-            plusMinusButton.style.display = 'inline-block';
-            treeItem.insertBefore(plusMinusButton, treeItem.childNodes[0]);
-            parent.appendChild(treeItem);
-
-            if (item.nodes != undefined) {
-                var childNodesContainer = document.createElement('div');
-                childNodesContainer.classList.add('TreeNode');
-                parent.appendChild(childNodesContainer);
-                plusMinusButton.innerText = '+';
-                registerClick(plusMinusButton, childNodesContainer);
-
-                childNodesContainer.style.display = 'none';
-
-                if (buildNodes(item.nodes, childNodesContainer)) {
-                    plusMinusButton.onclick();
-                    found = true;
                 }
-            }
-        }
+
+            });
         return found;
     };
     $.ajax({
         url: "../../menu.json",
         dataType: "json",
         success: function (data) {
+            var searchDiv = document.createElement('div');
+            searchDiv.style.padding = '10px';
+
+            var searchInput = document.createElement('input');
+            searchInput.style.width = '100%';
+            searchInput.placeholder = 'search';
+
+
+            searchDiv.appendChild(searchInput);
+            root.appendChild(searchDiv);
             buildNodes(data.nodes, root);
+            searchInput.oninput = function () {
+                if (searchInput.value == '')
+                    reset(data.nodes);
+                else
+                    search(data.nodes, searchInput.value);
+            };
 
         }
     });
